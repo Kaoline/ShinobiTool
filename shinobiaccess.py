c6cd20ee837acb45cc323b7bcc7508e9bee2746b
Kaoline <jujulableue@hotmail.com>
2017-11-06 00:56:39 +0100

    Bug fix

--- shinobiaccess.py
+++ shinobiaccess.py
@@ -84,7 +84,7 @@ class ShinobiAccess:
                 # clazz = tr.find(class_="village").previous_sibling.img["alt"]
                 sVillage = tr.find(class_="village").a.span.text
                 evo = int(tr.find(class_="evolution").text[1:])
-                points = float(tr.find(class_="points").text.replace(",", "."))
+                points = float(tr.find(class_="points").text.replace(",", ""))
                 if min_lvl <= lvl <= max_lvl and (village is None or sVillage == village.lower()) and min_score <= evo <= max_score and points >= min_points:
                     shinoobs.append(name)
                     # print("Page " + str(page_number) + " ok")
